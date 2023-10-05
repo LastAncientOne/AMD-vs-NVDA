46dceb0439afc0173a2e90c147d50b5ff729fed0
LastAncientOne <lastancientone@gmail.com>
2023-10-05 09:02:31 -0700

    Update Linear_Regresssion.R

--- Linear_Regresssion.R
+++ Linear_Regresssion.R
@@ -28,7 +28,6 @@ model_nvda <- lm(NVDA ~ SPY, data = data)
 summary(model_amd)
 summary(model_nvda)
 
-
 # Calculate residuals for both models
 residuals_amd <- residuals(model_amd)
 residuals_nvda <- residuals(model_nvda)
@@ -55,3 +54,19 @@ cat("\nNVDA Model:\n")
 cat("MAE:", mae_nvda, "\n")
 cat("MSE:", mse_nvda, "\n")
 cat("RMSE:", rmse_nvda, "\n")
+
+# Make predictions using the linear regression models
+predicted_amd <- predict(model_amd, newdata = data)
+predicted_nvda <- predict(model_nvda, newdata = data)
+
+# Create a new DataFrame with actual and predicted values
+prediction_df <- data.frame(
+  Date = index(data),
+  Actual_AMD = data$AMD,
+  Predicted_AMD = predicted_amd,
+  Actual_NVDA = data$NVDA,
+  Predicted_NVDA = predicted_nvda
+)
+
+# Print the DataFrame
+print(prediction_df)
